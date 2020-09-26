ba29171b2444df422e9a7112fc99ecf739112717
erocham <erocham@ucenfotec.ac.cr>
2020-09-25 22:16:10 -0600

    Mi primer commit - Tarea uno

--- src/mytestingapp/MyTestingApp.java
+++ src/mytestingapp/MyTestingApp.java
@@ -16,7 +16,7 @@ public class MyTestingApp {
      */
     public static void main(String[] args) {
         
-        
+        // COMENTARIO
        // MyForm  myForm = new MyForm();
         //myForm.setVisible(true);
         MyForm myForm = new MyForm();
