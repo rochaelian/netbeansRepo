4b73e59bd545155353a9a7a3a95d6cfd1d03bab3
erocham <erocham@ucenfotec.ac.cr>
2020-09-25 21:44:26 -0600

    Mi primer commit - Tarea 1

---
+++ src/mytestingapp/MyTestingApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytestingapp;
+
+/**
+ *
+ * @author Elian
+ */
+public class MyTestingApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+       // MyForm  myForm = new MyForm();
+        //myForm.setVisible(true);
+        MyForm myForm = new MyForm();
+        myForm.setVisible(true);
+    }
+    
+}
